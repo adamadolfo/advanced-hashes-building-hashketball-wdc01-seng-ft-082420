30379d42cc766171ab5315b30665281ab1ba9b0f
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 16:42:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,14 +39,14 @@ game_hash = {
     }, 
     {
       :player_name => "Mason Plumlee",
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks => 
+      :number => 1,
+      :shoe => 19,
+      :points => 26,
+      :rebounds => 11,
+      :assists => 6,
+      :steals => 3,
+      :blocks => 8,
+      :slam_dunks => 5
     }, 
     {
       :player_name => "",
