6e81421666603e93bd04004e5ae7188331d665a5
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 16:20:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,39 @@
 # Write your code here!
 game_hash = {
   :home => {
-    
+        :team_name => ""
+    :colors => ["", ""]
+    :players => [{:player_name => ""
+      :number => 
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists => 
+      :steals =>
+      :blocks =>
+      :slam_dunk =>
+    },{:player_name => ""
+      :number => 
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists => 
+      :steals =>
+      :blocks =>
+      :slam_dunk =>
+    },{:player_name => ""
+      :number => 
+      :shoe =>
+      :points =>
+      :rebounds =>
+      :assists => 
+      :steals =>
+      :blocks =>
+      :slam_dunk =>
+    },]
+  }
+}
+
   }
   
   :away => {
