889686defffc70c59536d93f1063bda535ecc2aa
Adam Adolfo <adamadolfo8@gmail.com>
2020-08-13 17:05:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,13 +2,13 @@
 def game_hash  
   {
     home: {
-      team_name: 
-      colors: 
+      team_name: , 
+      colors: ,
       player: 
     },
     away: {
-      team_name: 
-      colors: 
+      team_name: ,
+      colors: ,
       player: 
     }
   }
